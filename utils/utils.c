86aa2ce6857ae73f9206deadbdd4d029131e5f0f
Dania <daniaoulkadi20022@gmail.com>
2024-03-16 01:18:21 +0100

    Support for all special cases for generating the probability table

--- utils/utils.c
+++ utils/utils.c
@@ -4,13 +4,13 @@ Room allocMemoireRoom(int height, int width){
     int nbCol = width/DIM_CELL;
     int nblig = height/DIM_CELL;
     Room room;
-    room.mat = (int**)malloc(nblig * sizeof(int*));
+    room.mat = (State**)malloc(nblig * sizeof(State*));
     if(room.mat == NULL){
         printf("Erreur d'allocation mémoire !!\n");
         exit(1);
     }
     for(int i = 0; i < nblig; i ++){
-        room.mat[i] = (int*)malloc(nbCol * sizeof(int)); 
+        room.mat[i] = (State*)malloc(nbCol * sizeof(State)); 
         if(room.mat[i] == NULL){
             printf("Erreur d'allocation mémoire !!\n");
             exit(1);
@@ -18,6 +18,7 @@ Room allocMemoireRoom(int height, int width){
     }
     room.nbCol = nbCol;
     room.nbLig = nblig;
+    room.nbStats = nbCol * nblig;
     return room;
 }
 
@@ -31,67 +32,179 @@ int randomPosition(Room room){
 }
 
 // à voir sur les côté ce qui se passe 
-void generateTr(int nbState, int nbActions, float ***T, Room room){
-    for(int curentstate = 0; curentstate < nbState; curentstate ++){
-        for(int nextstate = 0; nextstate < nbState; nextstate ++){
-            float res = 0;
-            int action = 0;
-            if(nextstate == curentstate - room.nbCol){
-                    res = 0.5;
-            }else{
-                    if(nextstate == (curentstate - room.nbCol + 1) || nextstate == (curentstate - room.nbCol -1)){
-                        res = 0.25;
-                    }
-            } 
-            T[curentstate][action][nextstate] = res;
+void generateTr(int nbActions, float ***T, Room room){
 
-            action = 1;
-            res = 0;
-            if(nextstate == curentstate + room.nbCol){
-                    res = 0.5;
-            }else{
-                    if(nextstate == (curentstate + room.nbCol + 1) || nextstate == (curentstate + room.nbCol -1)){
-                        res = 0.25;
-                    }
-            } 
-            T[curentstate][action][nextstate] = res;
-
-
-            action = 2;
-            res = 0;
-                if(nextstate == curentstate + 1){
-                    res = 0.5;
-            }else{
-                    if(nextstate == (curentstate - room.nbCol + 1) || nextstate == (curentstate + room.nbCol + 1)){
-                        res = 0.25;
-                    }
-            } 
-            T[curentstate][action][nextstate] = res;
-
-
-            action = 3;
-            res = 0;
-            if(nextstate == curentstate - 1){
-                    res = 0.5;
-            }else{
-                    if(nextstate == (curentstate - room.nbCol - 1) || nextstate == (curentstate + room.nbCol - 1)){
-                        res = 0.25;
-                    }
+    for(int s = 0; s < room.nbStats; s ++){
+        for(int a = 0; a < nbActions; a ++){
+            for(int sf = 0; sf < room.nbStats; sf++){
+                T[s][a][sf] = 0;
             }
-            T[curentstate][action][nextstate] = res;
-
         }
     }
 
+    for(int curentstate = 0; curentstate < room.nbStats; curentstate ++){
+        if(curentstate == 0 || curentstate == room.nbCol-1){
+            
+            T[curentstate][0][curentstate] = 0.75;
+            T[curentstate][1][curentstate + room.nbCol] = 0.5;
+            T[curentstate][1][curentstate] = 0.25;
+
+            if(curentstate == 0){
+                T[curentstate][0][curentstate + 1] = 0.25;
+                T[curentstate][1][curentstate + room.nbCol + 1] = 0.25;
+                T[curentstate][2][curentstate + 1] = 0.5;
+                T[curentstate][2][curentstate] = 0.25;
+                T[curentstate][2][curentstate + room.nbCol + 1] = 0.25;
+                T[curentstate][3][curentstate] = 0.75;
+                T[curentstate][3][curentstate + room.nbCol] = 0.25;
+                
+            }else{
+                T[curentstate][0][curentstate - 1] = 0.25;
+                T[curentstate][1][curentstate + room.nbCol - 1] = 0.25;
+                T[curentstate][2][curentstate] = 0.75;
+                T[curentstate][2][curentstate + room.nbCol] = 0.25;
+                T[curentstate][3][curentstate - 1] = 0.5;
+                T[curentstate][3][curentstate] = 0.25;
+                T[curentstate][3][curentstate + room.nbCol - 1] = 0.25;
+            } 
+        }else{
+            if(curentstate == (room.nbLig-1)*room.nbCol || curentstate == room.nbStats -1){
+                T[curentstate][0][curentstate] = 0.25;
+                T[curentstate][0][curentstate - room.nbCol] = 0.5;
+                T[curentstate][1][curentstate] = 0.75;
+
+                if(curentstate == (room.nbLig-1)*room.nbCol){
+                    T[curentstate][0][curentstate - room.nbCol + 1] = 0.25;
+                    T[curentstate][1][curentstate + 1] = 0.25;
+                    T[curentstate][2][curentstate + 1] = 0.5;
+                    T[curentstate][2][curentstate] = 0.25;
+                    T[curentstate][2][curentstate - room.nbCol + 1] = 0.25;
+                    T[curentstate][3][curentstate] = 0.75;
+                    T[curentstate][3][curentstate - room.nbCol] = 0.25;
+
+                }else{
+                    T[curentstate][0][curentstate - room.nbCol - 1] = 0.25;
+                    T[curentstate][1][curentstate - 1] = 0.25;
+                    T[curentstate][2][curentstate] = 0.75;
+                    T[curentstate][2][curentstate - room.nbCol] = 0.25;
+                    T[curentstate][3][curentstate - 1] = 0.5;
+                    T[curentstate][3][curentstate] = 0.25;
+                    T[curentstate][3][curentstate - room.nbCol - 1] = 0.25;
+                }
+            }
+            else{
+                if(curentstate < room.nbCol - 1){
+                    int action = 0;
+                    T[curentstate][action][curentstate] = 0.5;
+                    T[curentstate][action][curentstate + 1] = 0.25;
+                    T[curentstate][action][curentstate - 1] = 0.25;
+                    action = 1;
+                    T[curentstate][action][curentstate + room.nbCol] = 0.5;
+                    T[curentstate][action][curentstate + room.nbCol + 1] = 0.25;
+                    T[curentstate][action][curentstate + room.nbCol - 1] = 0.25;
+                    action = 2;
+                    T[curentstate][action][curentstate + 1] = 0.5;
+                    T[curentstate][action][curentstate] = 0.25;
+                    T[curentstate][action][curentstate + room.nbCol + 1] = 0.25;
+                    action = 3;
+                    T[curentstate][action][curentstate - 1] = 0.5;
+                    T[curentstate][action][curentstate] = 0.25;
+                    T[curentstate][action][curentstate + room.nbCol - 1] = 0.25;
+
+                }else{
+                    if(curentstate%room.nbCol == 0){
+                            int action = 0;
+                            T[curentstate][action][curentstate] = 0.25;
+                            T[curentstate][action][curentstate -room.nbCol] = 0.5;
+                            T[curentstate][action][curentstate -room.nbCol + 1] = 0.25;
+                            action = 1;
+                            T[curentstate][action][curentstate + room.nbCol] = 0.5;
+                            T[curentstate][action][curentstate] = 0.25;
+                            T[curentstate][action][curentstate + room.nbCol + 1] = 0.25;
+                            action = 2;
+                            T[curentstate][action][curentstate + 1] = 0.5;
+                            T[curentstate][action][curentstate -room.nbCol + 1] = 0.25;
+                            T[curentstate][action][curentstate + room.nbCol + 1] = 0.25;
+                            action = 3;
+                            T[curentstate][action][curentstate - room.nbCol] = 0.25;
+                            T[curentstate][action][curentstate] = 0.5;
+                            T[curentstate][action][curentstate + room.nbCol ] = 0.25;
+                    }else{
+                            if((curentstate+1)%room.nbCol == 0){
+                                int action = 0;
+                                T[curentstate][action][curentstate - room.nbCol] = 0.5;
+                                T[curentstate][action][curentstate] = 0.25;
+                                T[curentstate][action][curentstate - room.nbCol - 1] = 0.25;
+                                action = 1;
+                                T[curentstate][action][curentstate] = 0.25;
+                                T[curentstate][action][curentstate + room.nbCol] = 0.5;
+                                T[curentstate][action][curentstate +room.nbCol - 1] = 0.25;
+                                action = 2;
+                                T[curentstate][action][curentstate] = 0.5;
+                                T[curentstate][action][curentstate + room.nbCol] = 0.25;
+                                T[curentstate][action][curentstate - room.nbCol] = 0.25;
+                                action = 3;
+                                T[curentstate][action][curentstate - 1] = 0.5;
+                                T[curentstate][action][curentstate + room.nbCol -1] = 0.25;
+                                T[curentstate][action][curentstate - room.nbCol - 1] = 0.25;
+                            }else{
+                                if((room.nbLig-1)*room.nbCol < curentstate && curentstate < room.nbStats - 1 ){
+                                    int action = 0;
+                                    T[curentstate][action][curentstate - room.nbCol] = 0.5;
+                                    T[curentstate][action][curentstate - room.nbCol + 1] = 0.25;
+                                    T[curentstate][action][curentstate - room.nbCol - 1] = 0.25;
+                                    action = 1;
+                                    T[curentstate][action][curentstate] = 0.5;
+                                    T[curentstate][action][curentstate + 1] = 0.25;
+                                    T[curentstate][action][curentstate - 1] = 0.25;
+                                    action = 2;
+                                    T[curentstate][action][curentstate + 1] = 0.5;
+                                    T[curentstate][action][curentstate] = 0.25;
+                                    T[curentstate][action][curentstate - room.nbCol + 1] = 0.25;
+                                    action = 3;
+                                    T[curentstate][action][curentstate - 1] = 0.5;
+                                    T[curentstate][action][curentstate] = 0.25;
+                                    T[curentstate][action][curentstate - room.nbCol - 1] = 0.25;
+                                }else{
+                                    printf("autes \n ");
+                                    int action = 0;
+                                    T[curentstate][action][curentstate - room.nbCol] = 0.5;
+                                    T[curentstate][action][curentstate - room.nbCol + 1] = 0.25;
+                                    T[curentstate][action][curentstate - room.nbCol - 1] = 0.25;
+                                    action = 1;
+                                    T[curentstate][action][curentstate + room.nbCol] = 0.5;
+                                    T[curentstate][action][curentstate + room.nbCol + 1] = 0.25;
+                                    T[curentstate][action][curentstate + room.nbCol - 1] = 0.25;
+                                    action = 2;
+                                    T[curentstate][action][curentstate + 1] = 0.5;
+                                    T[curentstate][action][curentstate + room.nbCol + 1] = 0.25;
+                                    T[curentstate][action][curentstate - room.nbCol + 1 ] = 0.25;
+                                    action = 3;
+                                    T[curentstate][action][curentstate - 1] = 0.5;
+                                    T[curentstate][action][curentstate + room.nbCol - 1] = 0.25;
+                                    T[curentstate][action][curentstate - room.nbCol - 1] = 0.25;
+                                }
+                            }
+                    
+                        }
+                }
+                }
+
+        }
+    }
 }
 
 
-void affichageT(float ***T, int nbstates, int nbactions){
-    for(int s = 0; s < nbstates; s ++){
+
+void affichageT(float ***T, Room room, int nbactions){
+    for(int s = 0; s < room.nbStats; s ++){
         printf("{\n");
         for(int a = 0; a < nbactions; a ++){
             printf("\t{ ");
-            for(int sf = 0; sf < nbstates; sf ++){
+            for(int sf = 0; sf < room.nbStats; sf ++){
+                if(sf%room.nbCol == 0){
+                    printf("\n\t");
+                }
                 printf("%0.2f, ", T[s][a][sf]);
             }
             printf("}\n");
@@ -125,14 +238,15 @@ float ***allocMemoireT(int nbStates, int nbActions){
 
 
 int main(int argc, char const *argv[])
-{
-    int nbstates = 16; 
+{ 
     int nbactions = 4;
-    Room room = allocMemoireRoom(120,120);
+    int height = 120;
+    int width = 120;
+    Room room = allocMemoireRoom(height,width);
     printf(" nbcol = %d, nblig = %d \n", room.nbCol, room.nbLig);
-    float ***T = allocMemoireT(nbstates, nbactions);
-    generateTr(nbstates, nbactions, T, room);
-    affichageT(T, nbstates, nbactions);
+    float ***T = allocMemoireT(room.nbStats, nbactions);
+    generateTr(nbactions, T, room);
+    affichageT(T, room, nbactions);
     return 0;
 }
 
