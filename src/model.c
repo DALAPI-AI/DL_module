ff4557791cc8e40a5f1f6a640066164c4f2dcc1f
Dania <daniaoulkadi20022@gmail.com>
2024-03-16 03:53:46 +0100

    Adapt model to the new structure

--- src/model.c
+++ src/model.c
@@ -3,10 +3,7 @@
 
 #define NB_ACTIONS 4
 #define R_LOOP 1
-#define R_HIT -10
-#define R_WIN 90
-// supposition 4 * 4
-#define NB_STATES 16 
+
 
 
 int actions[NB_ACTIONS] = {
@@ -15,15 +12,8 @@ int actions[NB_ACTIONS] = {
     0b10, // RIGHT / est 
     0b11  // LEFT / ouest
 };
-
-float Q [NB_STATES][NB_ACTIONS] = {0};
-
 float g = 0.95;
 
- // TODO : redefinir la stratégie de selon le modèle choisi 
-int R[NB_STATES][NB_ACTIONS][NB_STATES]={
-    
-};
 
 float max(float *tab, int nbElt){
     float max = tab[0];
@@ -35,19 +25,42 @@ float max(float *tab, int nbElt){
     return max;
 }
 
+int indexMax(float *tab, int nbElt){
+    int index = 0; 
+    float max = tab[0];
+    for(int i = 1; i < nbElt; i++){
+        if(max < tab[i]){
+            max = tab[i];
+            index = i;
+        }
+    }
+    return index;
+}
 
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
-    float learn = 0.1;
-    float exploration = 0.9;
-    int ***T = allocMemoireT(NB_STATES, NB_ACTIONS);
+
+    int height = 120;
+    int width = 120;
+    int finalState = 2;
+    Room room = allocMemoireRoom(height, width);
+
+    float ***T = allocMemoire3DimTab(room.nbStats, NB_ACTIONS);
+    generateTr(NB_ACTIONS, T, room, finalState);
+    affichageT(T, room, NB_ACTIONS);
+
+    float **Q = allocMemoireQ(NB_ACTIONS, room.nbStats);
+
+    float ***R = allocMemoire3DimTab(room.nbStats, NB_ACTIONS); 
+    generateR(NB_ACTIONS, R, room, finalState);
+    affichageT(R, room, NB_ACTIONS);
+
     for(int i = 0; i < 100; i++){    
-        for(int s = 0; s < NB_STATES; s++){
-            printf("\n");
+        for(int s = 0; s < room.nbStats; s++){
             for(int a = 0; a < NB_ACTIONS; a++){
                 float sum = 0;
-                for(int s = 0; s < NB_STATES; s++){
+                for(int s = 0; s < room.nbStats; s++){
                     sum += T[s][a][s] * (R[s][a][s] + g * max(Q[s], NB_ACTIONS));
                 }
                 Q[s][a] = sum;
@@ -55,7 +68,7 @@ int main(int argc, char const *argv[])
         }
 
         printf("Iteration: %d\n", i);
-        for(int s = 0; s < NB_STATES; s++){
+        for(int s = 0; s < room.nbStats; s++){
             printf("\n");
             for(int a = 0; a < NB_ACTIONS; a++){
                 printf("Q[State:%d, Action:%d]=%+10.4f", s, a, Q[s][a]);
@@ -68,6 +81,16 @@ int main(int argc, char const *argv[])
 
     } 
     printf("---------------------------------\n");  
+    printf("-----------------Result------------------ \n");
+    for(int s = 0; s < room.nbStats; s++){
+        if((s+1)%room.nbCol == 0){
+            printf("\n");
+        }
+        printf("%d ,",indexMax(Q[s], NB_ACTIONS));
+    }
+    printf("---------------------------------\n");
+    
+    
     return 0;
 }
 
--- utils/utils.h
+++ utils/utils.h
@@ -8,6 +8,10 @@
 #define DIM_CELL 30 // in centimeters 
 #define DIM_VECT 34 // 16(N) + 16(S) + 1(E) + 1(W) 
 
+
+#define R_HIT -1
+#define R_WIN 10
+
 typedef struct{
     int nbState;
     int wall;
@@ -31,7 +35,8 @@ Room allocMemoireRoom(int height, int width);
 int randomPosition(Room room);
 float ***allocMemoire3DimTab(int nbStates, int nbActions);
 void affichageT(float ***T, Room room, int nbactions);
-void generateTr(int nbActions, float ***T, Room room);
+void generateTr(int nbActions, float ***T, Room room, int finalState);
 float **allocMemoireQ(int nbActions, int nbStates);
+void generateR(int nbActions, float ***R, Room room, int succState);
 
 #endif
